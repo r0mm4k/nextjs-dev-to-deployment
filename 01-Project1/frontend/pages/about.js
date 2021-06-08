1f29b4f101d8ba15bcf7390e945dac0b6e979627
Roma Makarov <r0mmm4k@gmail.com>
2021-06-08 14:09:15 +0300

    pages and routing

---
+++ 01-Project1/frontend/pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ and other musical events</p>
+      <p>Virsion: 1.0.0</p>
+    </div>
+  )
+}
