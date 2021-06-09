ca9c38b9ce72731c77219dd3e6695d29a3afe4ea
Roma Makarov <r0mmm4k@gmail.com>
2021-06-09 15:57:52 +0300

    Single Event Page

--- 01-Project1/frontend/pages/events/[slug].js
+++ 01-Project1/frontend/pages/events/[slug].js
@@ -1,15 +1,52 @@
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 
 import { API_URL } from '@/config/index';
 
+import styles from '@/styles/event.module.css';
+
 import Layout from '@/components/layout';
 
-export default function EventPage({ event: { name } }) {
-  const { query: { slug } } = useRouter();
+export default function EventPage({ event: { id, name, date, time, image, performers, description, venue, address } }) {
+  const hasImage = image && (
+    <div className={styles.image}>
+      <Image src={image} width={960} height={600} />
+    </div>
+  );
+
+  const deleteEvent = () => console.log("delete event");
 
   return (
     <Layout>
-      <h1>{name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${id}`}>
+            <a><FaPencilAlt /> Edit Event</a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>{date} at {time}</span>
+        <h1>{name}</h1>
+
+        {hasImage}
+
+        <h3>Performers:</h3>
+        <p>{performers}</p>
+
+        <h3>Description:</h3>
+        <p>{description}</p>
+
+        <h3>Venue: {venue}</h3>
+        <p>{address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   )
 }
