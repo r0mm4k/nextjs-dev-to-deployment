33cdaa47b606e9f69afd25980212dfa4362ec5f9
Roma Makarov <r0mmm4k@gmail.com>
2021-06-11 13:10:46 +0300

    Search Component

--- 01-Project1/frontend/components/header.js
+++ 01-Project1/frontend/components/header.js
@@ -2,12 +2,16 @@ import Link from 'next/link';
 
 import styles from '@/styles/header.module.css';
 
+import Search from './search';
+
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">DJ Events</Link>
       </div>
+
+      <Search />
 
       <nav>
         <ul>
---
+++ 01-Project1/frontend/components/search.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+import styles from '@/styles/search.module.css';
+
+export default function Search() {
+  const [term, setTerm] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    push(`/events/search?term=${term}`);
+  };
+  const handleChange = ({ target: { value } }) => setTerm(value);
+
+  return (
+    <div className={styles.search}>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={term} onChange={handleChange} placeholder="Search Events" />
+      </form>
+    </div>
+  )
+}
